487c9a67bb7329d6539330ca9f8094b2c337c607
Quy <huynhminhquy1999@gmail.com>
2021-04-20 10:25:17 +0700

    users_decentralize

--- src/navigation/Routes.js
+++ src/navigation/Routes.js
@@ -1,8 +1,9 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import auth from '@react-native-firebase/auth';
+import db from '@react-native-firebase/database';
 import { AuthContext } from './AuthProvider';
-
+import {Text} from 'react-native';
 import AuthStack from './AuthStack';
 import AppStack from './AppStack';
 import AdminStack from './AdminStack';
@@ -12,23 +13,63 @@ const Routes = () => {
   const { user, setUser } = useContext(AuthContext);
   const [initializing, setInitializing] = useState(true);
 
-  const onAuthStateChanged = (txtuser) => {
-    console.log("txtuser", txtuser);
-    setUser(txtuser);
+//   async function createNewAccount() {
+//     try {
+//       // 3 types : [0 => admin, 1 => waiter, 2 => chef]
+//         const userAuth = await auth().createUserWithEmailAndPassword("hmq123452@gmail.com", "12345678");
+//         var user = {
+//             name: "Quy Minh Huynh",
+//             phone: "779797329",
+//             address: "474 Mercer Drive",
+//             uid: userAuth.user.uid,
+//             email: userAuth.user.email,
+//             active: true,
+//             type: 2
+//         }
+//         writeUserData(user)
+
+//     } catch (error) {
+//         console.log(error.message)
+//     }
+//   }
+
+//   function writeUserData(user) {
+//     db().ref('users/' + user.uid).set(user).catch(error => {
+//         console.log(error.message)
+//     });
+//  }
+
+  const getUserData = (user) => {
+    db().ref('users/' + user.uid).once("value", snap => {
+      setUser(snap.val());
+      console.log(snap.val())
+    })
+  }
+
+  const onAuthStateChanged = (user) => {
+    console.log("txtuser", user);
+    if(user !== null)
+      getUserData(user);
+    else
+    setUser(user);
     if (initializing) setInitializing(false);
   };
 
   useEffect(() => {
+    //createNewAccount();
+    console.log("Re-run when state children has changed")
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber; // unsubscribe on unmount
   }, []);
 
-  if (initializing) return null;
+  if (initializing) {console.log("chay 1");return null};
 
   return (
     <NavigationContainer>
-      {user ? <AdminStack /> : <AuthStack />}
-
+      {user === null ? <AuthStack/> : 
+      user.active && user.type === 0 ? <AdminStack/> : 
+      user.active && user.type === 1  ? <AppStack/> : 
+      <KitchenStack/>  }
     </NavigationContainer>
   );
 };
